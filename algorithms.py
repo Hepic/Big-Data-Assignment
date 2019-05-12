5bf21a0090cff6366a740c21078f2f35c4635ee2
Hepic <hepic135@gmail.com>
2019-05-12 12:25:04 +0300

    add criteria for ips

--- algorithms.py
+++ algorithms.py
@@ -4,7 +4,7 @@ import distances
 def find_par(parents, ind):
     if parents[ind] != ind:
         parents[ind] = find_par(parents, parents[ind])
-    
+
     return parents[ind]
 
 
@@ -18,36 +18,33 @@ def union_par(parents, ranks, ind1, ind2):
 
 
 def slow_algorithm(data):
-    mac_info, parents, ranks, families, mac_info_1, parents_1, ranks_1, works = dict(), dict(), dict(), dict(), dict(), dict(), dict(), dict()
-    
+    mac_info, parents, ranks, families = dict(), dict(), dict(), dict()
+
+    # retrieve mac addresses information
     for ind, row in data.iterrows():
         mac = int(row['MAC'])
         info = (row['Day'], row['Hour'], row['IP'])
-        
+
         if row['Day'] % 7 >= 1 and row['Day'] % 7 <= 5 and row['Hour'] >= 7 and row['Hour'] <= 16:
-            if mac not in mac_info_1:
-                mac_info_1[mac] = []
-                parents_1[mac] = mac
-                ranks_1[mac] = 0
+            continue
 
-            mac_info_1[mac].append(info)
-        else:
-            if mac not in mac_info:
-                mac_info[mac] = []
-                parents[mac] = mac
-                ranks[mac] = 0
+        if mac not in mac_info:
+            mac_info[mac] = []
+            parents[mac] = mac
+            ranks[mac] = 0
 
+        mac_info[mac].append(info)
+
+        for i in range(-2, 3, 1):
+            info = (row['Day'], (row['Hour'] + i + 24) % 24, row['IP'])
             mac_info[mac].append(info)
-            
-            for i in range(-2, 3, 1):
-                info = (row['Day'], (row['Hour'] + i + 24) % 24, row['IP'])
-                mac_info[mac].append(info)
 
+    # union find on closests mac addresses with jaccard distance
     for key1, value1 in mac_info.iteritems():
         for key2, value2 in mac_info.iteritems():
             if key1 == key2:
                 continue
-            
+
             dist = distances.jaccard_distance(value1, value2)
 
             if dist <= 0.88:
@@ -63,36 +60,31 @@ def slow_algorithm(data):
             families[par] = [key]
         else:
             families[par].append(key)
-        
-    ########################################
-    
-    for key1, value1 in mac_info_1.iteritems():
-        for key2, value2 in mac_info_1.iteritems():
-            if key1 == key2:
-                continue
-            
-            dist = distances.jaccard_distance(value1, value2)
 
-            if dist <= 0.6:
-                par1, par2 = find_par(parents_1, key1), find_par(parents_1, key2)
+    # remove some families based on some criteria
+    keyRemove = [key for key, value in families.iteritems() if len(value) <= 2]
 
-                if par1 != par2:
-                    union_par(parents_1, ranks_1, par1, par2)
+    for key, value in families.iteritems():
+        days_ips = [set() for i in range(50)]
 
-    for key, value in parents_1.iteritems():
-        par = find_par(parents_1, key)
+        for mac_num in value:
+            for (day, hour, ip) in mac_info[mac_num]:
+                days_ips[day].add(ip)
 
-        if par not in works:
-            works[par] = [key]
-        else:
-            works[par].append(key)
-    
-    for key, value in works.iteritems():
-        if len(value) >= 3:
-            for elem in value:
-                if elem in families:
-                    del families[elem][:]
+        sums, cnt = 0, 0
 
-    #########################################
+        for i in range(50):
+            if len(days_ips[i]):
+                sums += len(days_ips[i])
+                cnt += 1
+
+        avg = float(sums) / float(cnt)
+
+        if avg <= 4.0:
+            keyRemove.append(key)
+
+    for elem in keyRemove:
+        if elem in families:
+            del families[elem]
 
     return families
